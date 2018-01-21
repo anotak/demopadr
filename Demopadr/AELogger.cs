21634553fcd6375ebab1afeb257baf01a48721e0
anotak <takilgore@gmail.com>
2018-01-20 21:43:44 -0600

    first commit

---
+++ Demopadr/AELogger.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Diagnostics;
+namespace Demopadr
+{
+    public class AELogger
+    {
+        static List<String> Logfile = new List<string>(2048);
+
+        public static bool bLogging = true;
+        public static bool bPrintAll = false;
+
+        private static StringBuilder sbuilder = new StringBuilder(1024);
+
+        private static CultureInfo culture = CultureInfo.InvariantCulture;
+        public static Stopwatch stopwatch;
+
+#if DEBUG
+        public static bool bPrepared = false;
+#endif
+
+        public static void Prepare()
+        {
+#if DEBUG
+            bPrepared = true;
+#endif
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Log("logging started @ " + DateTime.Now.ToString(@"HH:mm:ss.fff", culture));
+            Log("working directory " + Directory.GetCurrentDirectory());
+            Log("build time: " + GetCompileDate());
+#if DEBUG
+            Log("WARNING: THIS IS A DEBUG BUILD");
+#endif
+        } // prepare()
+
+        public static void Log(string message, Boolean time = true, Boolean print = false)
+        {
+#if DEBUG
+            if (!bPrepared)
+            {
+                throw new Exception("AELogger.Prepare() not called yet!!!!!");
+            }
+#endif
+            if (bLogging)
+            {
+                if (time)
+                {
+                    // on .NET 4.0 we could
+                    //sbuilder.Clear();
+                    //sbuilder.Append(stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fff", culture));
+
+                    // but instead we must
+                    sbuilder.Length = 0;
+                    TimeSpan span = stopwatch.Elapsed;
+                    sbuilder.Append(
+                        String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                            span.TotalHours,
+                            span.TotalMinutes % 60.0,
+                            span.TotalSeconds % 60.0,
+                            span.TotalMilliseconds % 1000.0)
+                        );
+
+
+                    sbuilder.Append(": ");
+                    sbuilder.Append(message);
+
+                    Logfile.Add(sbuilder.ToString());
+
+                    if (print || bPrintAll)
+                    {
+                        Console.WriteLine(sbuilder.ToString());
+                    }
+                }
+                else
+                {
+                    Logfile.Add(message);
+                    if (print || bPrintAll)
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+
+            }
+        } // log string version
+
+        // STRINGBUILDER VERSION
+        public static void Log(StringBuilder inbuilder, Boolean time = true, Boolean print = false)
+        {
+#if DEBUG
+            if (!bPrepared)
+            {
+                throw new Exception("AELogger.Prepare() not called yet!!!!!");
+            }
+#endif
+            if (bLogging)
+            {
+                if (time)
+                {
+                    // on .NET 4.0 we could
+                    //sbuilder.Clear();
+                    //sbuilder.Append(stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fff", culture));
+
+                    // but instead we must
+                    sbuilder.Length = 0;
+                    TimeSpan span = stopwatch.Elapsed;
+                    sbuilder.Append(
+                        String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                            span.TotalHours,
+                            span.TotalMinutes % 60.0,
+                            span.TotalSeconds % 60.0,
+                            span.TotalMilliseconds % 1000.0)
+                        );
+
+
+                    sbuilder.Append(": ");
+                    sbuilder.Append(inbuilder);
+
+                    string message = sbuilder.ToString();
+                    Logfile.Add(message);
+
+                    if (print || bPrintAll)
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+                else
+                {
+                    string message = inbuilder.ToString();
+                    Logfile.Add(message);
+                    if (print || bPrintAll)
+                    {
+                        Console.WriteLine(message);
+                    }
+                }
+
+            }
+        } // log stringbuilder
+
+        public static void Print(string message)
+        {
+            Log(message, false, true);
+        }
+
+        public static string GetCompileDate()
+        {
+            System.Version MyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            return new DateTime(2000, 1, 1).AddDays(MyVersion.Build).AddSeconds(MyVersion.Revision * 2).ToString();
+        }
+
+        public static void WriteLog(string filename = "")
+        {
+            if (bLogging)
+            {
+                if (filename == "")
+                {
+                    filename = System.Reflection.Assembly.GetCallingAssembly().GetName().Name + "-logfile.txt";
+                }
+
+                using (StreamWriter SW = File.CreateText(filename))
+                {
+                    int count = Logfile.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        SW.WriteLine(Logfile[i]);
+                    }
+                    SW.WriteLine("Logfile written @ " + DateTime.Now.ToString(@"HH:mm:ss.fff", culture));
+                } // closed
+            }
+        } // writelog
+
+    } // class
+} // ns
---
+++ Demopadr/Program.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Demopadr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AELogger.Prepare();
+            try
+            {
+                Run(args);
+            }
+            catch (Exception e)
+            {
+                AELogger.Print("Exception: " + e.Message);
+
+                AELogger.Log("Exception: " + e.StackTrace);
+
+                int i = 1;
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    AELogger.Log("InnerException " + i + ": " + e.Message);
+
+                    AELogger.Log("InnerException " + i + ": " + e.StackTrace);
+                    i++;
+                }
+                Console.WriteLine(e.Message);
+
+                AELogger.Print("Got a big error, if it keeps happening,\nseek out anotak for assistance (and bring the logfile.txt)");
+            }
+
+            AELogger.WriteLog();
+        } // main
+
+        static void Run(string[] args)
+        {
+            AELogger.Print("demopadr - a tool by anotak to pad demo lumps");
+            AELogger.Print("           for use in vanilla Doom's attract mode");
+            AELogger.Print("           sequence. for more information see");
+            AELogger.Print("https://doomwiki.org/wiki/Revenant_tracers_desync_internal_demos");
+            AELogger.Print("");
+
+            if (args.Length < 2)
+            {
+                AELogger.Print("\nNo file names specified.");
+                AELogger.Print("Correct usage:");
+                AELogger.Print("\tdemopadr <input> <output>");
+                AELogger.Print("\t(warning: this overwrites the output file!)");
+                AELogger.Print("");
+
+                return;
+            }
+
+            if (args[0].ToLower() != "-b")
+            {
+                string in_filename = args[0];
+                string out_filename = args[1];
+
+                PadDemo(in_filename, out_filename);
+            }
+            else
+            {
+                if (args.Length < 3)
+                {
+                    AELogger.Print("\nNo directory names specified for batch mode.");
+                    AELogger.Print("Correct usage:");
+                    AELogger.Print("\tdemopadr -b <input> <output>");
+                    AELogger.Print("\t(warning: this overwrites the output files!)");
+                    AELogger.Print("");
+
+                    return;
+                }
+
+                string dir = args[1];
+                string out_dir = args[2];
+
+                BatchDirectory(dir,out_dir);
+            }
+        } // run(
+
+        static void BatchDirectory(string in_dir, string out_dir)
+        {
+            if (!Directory.Exists(in_dir))
+            {
+                AELogger.Print(in_dir + " is not a valid directory, please specify a valid directory");
+                return;
+            }
+
+            if (!Directory.Exists(out_dir))
+            {
+                Directory.CreateDirectory(out_dir);
+            }
+
+            foreach (string in_filename in Directory.GetFiles(in_dir))
+            {
+                AELogger.Print("---------------------");
+                string out_filename = Path.Combine(out_dir, Path.GetFileName(in_filename));
+                AELogger.Print("doing " + in_filename + " to " + out_filename);
+
+                PadDemo(in_filename, out_filename);
+            }
+        }
+        
+        static void PadDemo(string in_filename, string out_filename)
+        {
+            if (!File.Exists(in_filename))
+            {
+                AELogger.Print("File " + in_filename + " doesn't exist!");
+                return;
+            }
+
+            AELogger.Print("Loading " + in_filename);
+
+            // demo file structure is
+            // 13 byte header
+            // 4 bytes per tic
+            // single 0x80 byte at end
+
+            // longtics uses 5 bytes per tic
+
+
+            int filesize;
+            int original_num_tics;
+            int target_num_tics;
+            int tic_size = 4;
+            byte[] output;
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(in_filename)))
+            {
+                byte version = reader.ReadByte(); // do not forget this value
+
+                if (version == 111) // is longtics?
+                {
+                    tic_size = 5;
+                    AELogger.Print("longtics demo detected");
+                }
+                else
+                {
+                    tic_size = 4;
+                }
+
+                filesize = (int)reader.BaseStream.Length;
+
+                byte[] input = reader.ReadBytes(filesize - 1);
+
+                int orig_footer_ptr = 0;
+
+                for (int i = 12; i < filesize - 1; i += tic_size)
+                {
+                    if (input[i] == 0x80)
+                    {
+                        orig_footer_ptr = i + 1;
+                        AELogger.Log("file ends at " + orig_footer_ptr);
+                        break; // for
+                    }
+                }
+
+                int footer_size = 0;
+
+                if (orig_footer_ptr == 0)
+                {
+                    AELogger.Print("warning, 0x80 not found, attempting to proceed anyway.");
+                    AELogger.Print("this may be an error (?)");
+                    orig_footer_ptr = filesize - 1;
+                    original_num_tics = (filesize - 14) / tic_size;
+                }
+                else
+                {
+                    if (orig_footer_ptr + 1 < filesize)
+                    {
+                        footer_size = filesize - (orig_footer_ptr + 1);
+                        AELogger.Print("preserving footer with size " + footer_size);
+                    }
+                    original_num_tics = (orig_footer_ptr - 13) / tic_size;
+                }
+
+                AELogger.Print("original_num_tics: " + original_num_tics);
+
+                // determine padding
+                if (original_num_tics % 4 == 0)
+                {
+                    target_num_tics = original_num_tics;
+                }
+                else
+                {
+                    target_num_tics = (original_num_tics) + (4 - (original_num_tics % 4));
+                }
+
+                AELogger.Print("target_num_tics: " + target_num_tics);
+
+                int tgt_footer_ptr = 14 + (target_num_tics * tic_size);
+
+                output = new byte[tgt_footer_ptr + footer_size];
+
+                output[0] = version;
+
+                AELogger.Log("filesize " + filesize + " vs output size " + output.Length);
+                AELogger.Log("doing main copy");
+                Array.Copy(input, 0, output, 1, orig_footer_ptr-1);
+                
+                if (footer_size > 0)
+                {
+                    AELogger.Log("doing footer copy w size " + footer_size);
+                    Array.Copy(input, orig_footer_ptr, output, tgt_footer_ptr, footer_size);
+                }
+
+                AELogger.Log("writing 0x80 @ " + (tgt_footer_ptr - 1));
+                output[tgt_footer_ptr - 1] = 0x80; // final ending byte
+            } // using
+
+            AELogger.Print("writing to temp file DEMOPADR.TMP");
+            if (File.Exists("DEMOPADR.TMP"))
+            {
+                File.Delete("DEMOPADR.TMP");
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite("DEMOPADR.TMP")))
+            {
+                writer.Write(output);
+            }
+
+            if (File.Exists(out_filename))
+            {
+                AELogger.Print("copying temp file over existing " + out_filename);
+                File.Delete(out_filename);
+            }
+            else
+            {
+                AELogger.Print("writing new " + out_filename);
+            }
+
+            File.Copy("DEMOPADR.TMP", out_filename);
+
+            try
+            {
+                if (File.Exists("DEMOPADR.TMP"))
+                {
+                    File.Delete("DEMOPADR.TMP");
+                }
+            }
+            catch (Exception e)
+            {
+                AELogger.Print("warning: there was some kind of error deleting DEMOPADR.TMP");
+                AELogger.Print("check Logfile.txt for more information?");
+
+                AELogger.Log("Exception: " + e.Message);
+
+                AELogger.Log("Exception: " + e.StackTrace);
+
+                int i = 1;
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    AELogger.Log("InnerException " + i + ": " + e.Message);
+
+                    AELogger.Log("InnerException " + i + ": " + e.StackTrace);
+                    i++;
+
+                }
+            } 
+
+        } // paddemo(
+
+    } // class
+} // ns
